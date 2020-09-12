32a23608774edd4f6518addeb8838206cda582cb
Kasun Nirmala <bgkasunnirmala@gmail.com>
2020-09-12 18:23:11 +0530

    date changes

--- routes/deviceData.js
+++ routes/deviceData.js
@@ -25,12 +25,12 @@ router.get('/byBatchAndID/:nodeID/:batch_id', async (req, res) => {
 
 
 router.get('/getWithering/:date', async (req, res) => {
-    var end = moment(moment(req.params.date).tz("Asia/Colombo").format("YYYY-MM-DD").toString()+" 8:00:00");
-    var start = moment(moment(req.params.date).add(-1,'day').tz("Asia/Colombo").format("YYYY-MM-DD").toString() + " 18:00:00");
+    var end = moment(moment(req.params.date).format("YYYY-MM-DD").toString() + " 8:00:00").toISOString(true);
+    var start = moment(moment(req.params.date).add(-1, 'day').format("YYYY-MM-DD").toString() + " 18:00:00").toISOString(true);;
    
-
+// res.json({end:end,start:start});
     try {
-        var DeviceData = await DeviceDataModel.find({ "created_on": { "$gte": start, "$lt":end } });
+        var DeviceData = await DeviceDataModel.find({ "created_on": { "$gte": new Date(start), "$lt":new Date(end) } });
         res.json(DeviceData);
     } catch (error) {
         res.json({ message: error.message });
